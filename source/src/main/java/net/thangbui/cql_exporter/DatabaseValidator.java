3d1e6ce5ef124fb3303b1300b60d8ab6d2df6e7b
thang <thang.buinguyen@yahoo.com.vn>
2016-09-12 14:59:46 +0800

    change email

--- source/src/main/java/net/thangbui/cql_exporter/DatabaseValidator.java
+++ source/src/main/java/net/thangbui/cql_exporter/DatabaseValidator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, Bui Nguyen Thang, thang.buinguyen@gmail.com, thangbui.net. All rights reserved.
+ * Copyright (c) 2016, Bui Nguyen Thang, thang.bn@live.com, thangbui.net. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
